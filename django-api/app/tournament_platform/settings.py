d55215ce8558b958ffbdce8a8f12aea706b30182
Renzo Ramos <renzoramosivan@gmail.com>
2026-05-21 12:08:17 +0200

    feat(settings): agregar django-filter y configurar backends de filtrado
    - Instala django-filter>=24.0
    - Configura DjangoFilterBackend en REST_FRAMEWORK.DEFAULT_FILTER_BACKENDS
    - Agrega django_filters a INSTALLED_APPS

--- django-api/app/tournament_platform/settings.py
+++ django-api/app/tournament_platform/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
     "corsheaders",
     "core",
 ]
@@ -83,6 +84,9 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": config("PAGE_SIZE", default=20, cast=int),
